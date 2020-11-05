c721c64a8ed88b2a1d840a845392b65137635a41
Fricilia <73948486+Fricilia@users.noreply.github.com>
2020-11-05 16:23:44 +0700

    Update Python.py

--- Python.py
+++ Python.py
@@ -26,6 +26,7 @@ def luas_lingkaran(radius) :
 def keliling_lingkaran(radius) :
     keliling = 2 * 3.14 * radius
     return keliling
+
 pilihan = 1
 while pilihan !=0:
     print('PEMBELAJARAN MATEMATIKA SEDERHANA')
@@ -39,3 +40,28 @@ while pilihan !=0:
     print('7. Luas Lingkaran')
     print('8. Keliling Lingkaran')
     print('0. exit')
+
+    pilihan = int(input('masukkan nomor opsi :'))
+    if pilihan ==1 :
+        print('\n')
+        print('Luas bujur sangkar')
+        sisi = int(input('sisi :'))
+        print('Luas bujur sangkar :%d\n' %luas_bujur_sangkar(sisi))
+    elif pilihan ==2 :
+        print('\n')
+        print('Keliling bujur sangkar')
+        sisi = int(input('sisi :'))
+        print('Keliling bujur sangkar :%d\n' %keliling_bujur_sangkar(sisi))
+    elif pilihan ==3 :
+        print('\n')
+        print('Luas persegi panjang')
+        panjang = int(input('panjang: '))
+        lebar = int(input('lebar :'))
+        print('Luas persegi panjang :%d\n' %luas_persegi_panjang(panjang,lebar))
+    elif pilihan ==4 :
+        print('\n')
+        print('Keliling persegi panjang')
+        panjang = int(input('panjang :'))
+        lebar = int(input('lebar :'))
+        print('Keliling persegi panjang :%d\n' %keliling_persegi_panjang(panjang,lebar))
+    
