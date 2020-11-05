a5dfb8ac19ee5da6b06eeca28369239e0b541627
Fricilia <73948486+Fricilia@users.noreply.github.com>
2020-11-05 16:02:55 +0700

    Update Python.py

--- Python.py
+++ Python.py
@@ -11,3 +11,6 @@ def keliling_bujur_sangkar(sisi) :
 def luas_persegi_panjang(panjang,lebar) :
     luas = panjang * lebar
     return luas
+def keliling_persegi_panjang(panjang,lebar):
+    keliling = 2 * (panjang + lebar) 
+    return keliling
